77c418b174d6b03376fd113fbf9b6d32ccf38103
kartikkwatra <kwatra.kartik@gmail.com>
2020-05-10 14:46:48 +0530

    final

--- poptimes/__init__.py
+++ poptimes/__init__.py
@@ -46,11 +46,11 @@ def get(api_key, types, p1, p2, n_threads=20, radius=180, all_places=False):
     return run(params)
 
 
-def get_id(api_key, place_id):
+def get_id(api_key, place_id , name, address):
     """
     retrieves the current popularity for a given place
     :param api_key:
     :param place_id:
     :return: see readme
     """
-    return get_populartimes(api_key, place_id)
+    return get_populartimes(api_key, place_id, name, address)
--- poptimes/crawler.py
+++ poptimes/crawler.py
@@ -331,6 +331,42 @@ def add_optional_parameters(detail_json, detail, rating, rating_n, popularity, c
     return detail_json
 
 
+def add_optional_parameters_sans(detail_json, rating, rating_n, popularity, current_popularity, time_spent):
+    """
+    check for optional return parameters and add them to the result json
+    :param detail_json:
+    :param rating:
+    :param rating_n:
+    :param popularity:
+    :param current_popularity:
+    :param time_spent:
+    :return:
+    """
+
+    if rating:
+        detail_json["rating"] = rating
+
+    if rating_n:
+        detail_json["rating_n"] = rating_n
+
+    if current_popularity:
+        detail_json["current_popularity"] = current_popularity
+
+    if popularity:
+        popularity, wait_times = get_popularity_for_day(popularity)
+
+        detail_json["populartimes"] = popularity
+
+        if wait_times:
+            detail_json["time_wait"] = wait_times
+
+    if time_spent:
+        detail_json["time_spent"] = time_spent
+
+    return detail_json
+
+
+
 def get_populartimes_from_search(place_identifier):
     """
     request information for a place and parse current popularity
@@ -383,17 +419,11 @@ def get_populartimes_from_search(place_identifier):
 
 
     jdata = json.loads(data)["d"]
-    print( jdata )
 
     jdata = json.loads(jdata[4:])
 
     # get info from result array, has to be adapted if backend api changes
     info = index_get(jdata, 0, 1, 0, 14)
-
-    print( type (info) )
-
-    pd.DataFrame(info).to_csv("output.csv",index=False)
-
 
     rating = index_get(info, 4, 7)
     rating_n = index_get(info, 4, 8)
@@ -422,9 +452,103 @@ def get_populartimes_from_search(place_identifier):
 
         time_spent = [int(t) for t in time_spent]
 
-    #TODO restore
+
+    # detail_json = add_optional_parameters_sans(detail_json, rating, rating_n, popular_times, current_popularity, time_spent )
+
+    # return detail_json
+    return rating, rating_n, popular_times, current_popularity, time_spent
+
+def get_populartimes_from_search_spl(place_identifier, detail_json):
+    """
+    request information for a place and parse current popularity
+    :param place_identifier: name and address string
+    :return:
+    """
+    params_url = {
+        "tbm": "map",
+        "tch": 1,
+        "hl": "en",
+        "q": urllib.parse.quote_plus(place_identifier),
+        "pb": "!4m12!1m3!1d4005.9771522653964!2d-122.42072974863942!3d37.8077459796541!2m3!1f0!2f0!3f0!3m2!1i1125!2i976"
+              "!4f13.1!7i20!10b1!12m6!2m3!5m1!6e2!20e3!10b1!16b1!19m3!2m2!1i392!2i106!20m61!2m2!1i203!2i100!3m2!2i4!5b1"
+              "!6m6!1m2!1i86!2i86!1m2!1i408!2i200!7m46!1m3!1e1!2b0!3e3!1m3!1e2!2b1!3e2!1m3!1e2!2b0!3e3!1m3!1e3!2b0!3e3!"
+              "1m3!1e4!2b0!3e3!1m3!1e8!2b0!3e3!1m3!1e3!2b1!3e2!1m3!1e9!2b1!3e2!1m3!1e10!2b0!3e3!1m3!1e10!2b1!3e2!1m3!1e"
+              "10!2b0!3e4!2b1!4b1!9b0!22m6!1sa9fVWea_MsX8adX8j8AE%3A1!2zMWk6Mix0OjExODg3LGU6MSxwOmE5ZlZXZWFfTXNYOGFkWDh"
+              "qOEFFOjE!7e81!12e3!17sa9fVWea_MsX8adX8j8AE%3A564!18e15!24m15!2b1!5m4!2b1!3b1!5b1!6b1!10m1!8e3!17b1!24b1!"
+              "25b1!26b1!30m1!2b1!36b1!26m3!2m2!1i80!2i92!30m28!1m6!1m2!1i0!2i0!2m2!1i458!2i976!1m6!1m2!1i1075!2i0!2m2!"
+              "1i1125!2i976!1m6!1m2!1i0!2i0!2m2!1i1125!2i20!1m6!1m2!1i0!2i956!2m2!1i1125!2i976!37m1!1e81!42b1!47m0!49m1"
+              "!3b1"
+    }
+
+    search_url = "https://www.google.de/search?" + "&".join(k + "=" + str(v) for k, v in params_url.items())
+    logging.info("searchterm: " + search_url)
+
+    # noinspection PyUnresolvedReferences
+    gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+
+
+    resp = urllib.request.urlopen(urllib.request.Request(url=search_url, data=None, headers=USER_AGENT),
+                                  context=gcontext)
+    data = resp.read().decode('utf-8').split('/*""*/')[0]
+
+
+    # with open('f1.txt','w') as f:
+    #     f.write('\n'.join( resp.read()  ))
+
+    # with open('f2.txt','w') as f:
+    #     f.write('\n'.join(  resp.read().decode('utf-8') ))
+    
+    
+    # pd.DataFrame(resp.read()).to_csv("respUndecoded.csv",index=False)
+    # pd.DataFrame(resp.read().decode('utf-8')).to_csv("respDecoded.csv",index=False)
+
+
+    # find eof json
+    jend = data.rfind("}")
+    if jend >= 0:
+        data = data[:jend + 1]
+
+
+    jdata = json.loads(data)["d"]
+
+    jdata = json.loads(jdata[4:])
+
+    # get info from result array, has to be adapted if backend api changes
+    info = index_get(jdata, 0, 1, 0, 14)
+
+    rating = index_get(info, 4, 7)
+    rating_n = index_get(info, 4, 8)
+
+    popular_times = index_get(info, 84, 0)
+
+    # current_popularity is also not available if popular_times isn't
+    current_popularity = index_get(info, 84, 7, 1)
+
+    time_spent = index_get(info, 117, 0)
+
+    # extract wait times and convert to minutes
+    if time_spent:
+
+        nums = [float(f) for f in re.findall(r'\d*\.\d+|\d+', time_spent.replace(",", "."))]
+        contains_min, contains_hour = "min" in time_spent, "hour" in time_spent or "hr" in time_spent
+
+        time_spent = None
+
+        if contains_min and contains_hour:
+            time_spent = [nums[0], nums[1] * 60]
+        elif contains_hour:
+            time_spent = [nums[0] * 60, (nums[0] if len(nums) == 1 else nums[1]) * 60]
+        elif contains_min:
+            time_spent = [nums[0], nums[0] if len(nums) == 1 else nums[1]]
+
+        time_spent = [int(t) for t in time_spent]
+
+
+    detail_json = add_optional_parameters_sans(detail_json, rating, rating_n, popular_times, current_popularity, time_spent )
+
+    return detail_json
     # return rating, rating_n, popular_times, current_popularity, time_spent
-    return current_popularity
+
 
 
 def get_detail(place_id):
@@ -441,7 +565,8 @@ def get_detail(place_id):
         results.append(detail_json)
 
 
-def get_populartimes(api_key, place_id):
+
+def get_populartimes(api_key, place_id , name, address):
     """
     sends request to detail to get a search string
     and uses standard proto buffer to get additional information
@@ -451,9 +576,9 @@ def get_populartimes(api_key, place_id):
 
     # places api - detail search
     # https://developers.google.com/places/web-service/details?hl=de
-    detail_str = DETAIL_URL.format(place_id, api_key)
+    # detail_str = DETAIL_URL.format(place_id, api_key)
     
-    print("in get_populartimes")
+    # print("in get_populartimes")
 
     # resp = json.loads(requests.get(detail_str, auth=('user', 'pass')).text)
     # check_response_code(resp)
@@ -461,18 +586,24 @@ def get_populartimes(api_key, place_id):
 
     # return get_populartimes_by_detail(api_key, detail)
 
-    name = "Reliance SMART"
-    address = "Johnson Dye Compound, West, 12, Ramchandra Ln, Malad, opp Paras industry, near IDBI Bank, Malad, Navy Colony, Kanchpada, Malad West, Mumbai, Maharashtra 400064, India"
+    # name = "Reliance SMART"
+    # address = "Johnson Dye Compound, West, 12, Ramchandra Ln, Malad, opp Paras industry, near IDBI Bank, Malad, Navy Colony, Kanchpada, Malad West, Mumbai, Maharashtra 400064, India"
     place_identifier = "{} {}".format(name, address)
 
-    return get_populartimes_from_search(place_identifier)
+    detail_json = {
+        "id": place_id,
+        "name": name,
+        "address": address
+    }
+
+    return get_populartimes_from_search_spl(place_identifier, detail_json)
 
 
 def get_populartimes_by_detail(api_key, detail):
     address = detail["formatted_address"] if "formatted_address" in detail else detail.get("vicinity", "")
     
-    print( detail["name"] )
-    print( address )
+    # print( detail["name"] )
+    # print( address )
     
     place_identifier = "{} {}".format(detail["name"], address)
 
